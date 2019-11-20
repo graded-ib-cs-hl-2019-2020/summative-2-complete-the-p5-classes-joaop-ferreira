02ba347e7863cf5a358bd2c4051f3fa560a98602
Joao <gr221030@graded.br>
2019-11-20 17:55:25 -0300

    enter

--- src/functions.ts
+++ src/functions.ts
@@ -1,2 +1,17 @@
 let canvas: HTMLCanvasElement;
 let canvas_context: CanvasRenderingContext2D;
+
+let width: number;
+let height: number;
+
+function createCanvas(width: number, height: number) {
+    canvas = <HTMLCanvasElement>document.getElementById('canvas');
+
+    canvas.width = width;
+    canvas.height = height;
+
+    width = width;
+    height = height;
+
+    canvas_context = <CanvasRenderingContext2D>canvas.getContext("2d");
+}
